c7292c7189387e55051f7afedc40a0419802fc36
todori <ionut.todoran@msg.group>
2023-04-10 17:27:44 +0300

    security 5

--- shop/src/main/java/ro/msg/learning/shop/service/auth/JwtService.java
+++ shop/src/main/java/ro/msg/learning/shop/service/auth/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "3F4528482B4D6251655468566D597133743677397A24432646294A404E635266";
+    @Value("${security.secret.key}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
